aa74a250cb3d0aa5ed11d201d201f30b48f818bf
0Shark <47896217+0Shark@users.noreply.github.com>
2021-01-30 14:22:00 +0100

    Weather preview

--- server.py
+++ server.py
@@ -2,6 +2,11 @@ from flask import Flask, render_template, request
 import pymysql
 import datetime
 import utils
+from pyowm import OWM
+
+# OpenWeather
+weatherAPI_token = "44edc82d5c54a7d0cd68aec1904e810e"
+mgr = OWM(weatherAPI_token)
 
 # initializing variables
 s = 0
@@ -54,7 +59,8 @@ def bot_list():  # extracting bot responses from user_bot_chat database
 
 @app.route('/home')  # links to the first page - index.html
 def index():
-    return render_template("index.html")
+    weather = getWeather()
+    return render_template("index.html", user_input=r(), temp=weather[0], location=weather[1], icon=weather[2], humidity=weather[3], wind=weather[4])
 
 
 @app.route('/')  # links to the first page - index.html
@@ -64,17 +70,19 @@ def home():
 
 @app.route('/setup', methods=['POST'])
 def setup():
+    weather = getWeather()
     global facehash
     facehash= request.form["facehash"]
-    return render_template("index.html")
+    return render_template("index.html", user_input=r(), temp=weather[0], location=weather[1], icon=weather[2], humidity=weather[3], wind=weather[4])
 
 
 @app.route('/clear')
 def clearChat():
+    weather = getWeather()
     # Clear all table rows
     sql = "TRUNCATE TABLE user_bot_chat;"
     a.execute(sql)
-    return render_template("index.html")
+    return render_template("index.html", user_input=r(), temp=weather[0], location=weather[1], icon=weather[2], humidity=weather[3], wind=weather[4])
 
 
 def r():  # takes user inputs and bot outputs and insert into a array to later send to html file
@@ -98,10 +106,23 @@ def r():  # takes user inputs and bot outputs and insert into a array to later s
         return r
 
 
+def getWeather():
+    observation = mgr.weather_at_place('Shkodër')
+    w = observation.get_weather()
+    state = w.get_detailed_status()
+    wind_data = w.get_wind()
+    humidity = w.get_humidity()
+    temp_data = w.get_temperature('celsius')
+    icon = w.get_weather_icon_name()
+    weatherData = [str(int(temp_data['temp'])), 'Shkodër', str(icon), str(int(humidity)), str(int(wind_data['speed']))]
+    return weatherData
+
+
 @app.route('/process', methods=['POST'])
 def process():
+    weather = getWeather()
     # called when user input is given and submit button is pressed
-    return render_template("index.html", user_input=r())
+    return render_template("index.html", user_input=r(), temp=weather[0], location=weather[1], icon=weather[2], humidity=weather[3], wind=weather[4])
 
 
 if __name__ == '__main__':
@@ -112,6 +133,6 @@ if __name__ == '__main__':
         print("QUERY ERROR: Connection")
         print("Exeception occured:{}".format(e))
 
-    app.run(host='127.0.0.1', port=int('8000'), debug=True)  # 0.0.0.0.,80
-    conn.close()
-    a.close()
+    app.run(host='0.0.0.0', port=int('8000'), debug=True)  # 0.0.0.0.,80
+    # conn.close()
+    # a.close()
